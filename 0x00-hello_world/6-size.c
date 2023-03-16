1016b723f5f7e2346174d31741eb940e355998f6
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-16 11:19:31 +0100

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@ int main(void)
   long long d;
   float e;
   printf("Size of a char: %lu byte(s)\n", sizeof(a));
-  printf("Size of a int: %i byte(s)\n", sizeof(b));
+  printf("Size of an int: %lu byte(s)\n", sizeof(b));
   printf("Size of a long int: %lu byte(s)\n", sizeof(c));
   printf("Size of a long long: %lu byte(s)\n", sizeof(d));
   printf("Size of a float: %lu byte(s)\n", sizeof(e));
   return (0)
-;}
+}
