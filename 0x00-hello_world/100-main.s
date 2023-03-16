f73f1e1d023af8edc45caa23df8553d5e5ed9798
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-16 11:52:23 +0100

    Intel Syntax

--- 0x00-hello_world/100-main.s
+++ 0x00-hello_world/100-main.s
@@ -1,30 +1,34 @@
 	.file	"100-main.c"
 	.intel_syntax noprefix
+# GNU C17 (Debian 12.2.0-14) version 12.2.0 (x86_64-linux-gnu)
+#	compiled by GNU C version 12.2.0, GMP version 6.2.1, MPFR version 4.1.1-p1, MPC version 1.3.1, isl version isl-0.25-GMP
+
+# warning: MPFR header version 4.1.1-p1 differs from library version 4.2.0.
+# GGC heuristics: --param ggc-min-expand=100 --param ggc-min-heapsize=131072
+# options passed: -masm=intel -mtune=generic -march=x86-64 -Og -fasynchronous-unwind-tables
 	.text
-	.section	.rodata
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
 	.string	"Holberton School"
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB11:
 	.cfi_startproc
-	push	rbp
+	sub	rsp, 8	#,
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	mov	rbp, rsp
-	.cfi_def_cfa_register 6
-	lea	rax, .LC0[rip]
-	mov	rdi, rax
-	mov	eax, 0
-	call	printf@PLT
-	mov	eax, 0
-	pop	rbp
-	.cfi_def_cfa 7, 8
-	ret
+# 100-main.c:10: 	printf("Holberton School");
+	lea	rdi, .LC0[rip]	# tmp83,
+	mov	eax, 0	#,
+	call	printf@PLT	#
+# 100-main.c:12: }
+	mov	eax, 0	#,
+	add	rsp, 8	#,
+	.cfi_def_cfa_offset 8
+	ret	
 	.cfi_endproc
-.LFE0:
+.LFE11:
 	.size	main, .-main
 	.ident	"GCC: (Debian 12.2.0-14) 12.2.0"
 	.section	.note.GNU-stack,"",@progbits
