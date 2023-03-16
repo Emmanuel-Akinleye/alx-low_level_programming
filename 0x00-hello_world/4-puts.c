687a46304dd5bab6c55888d4ee62478df5e9f7e3
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-16 10:24:01 +0100

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char text[100];
 
-	strcpy(text, "Programming is like building a multilingual puzzle\n");
+	strcpy(text, \""Programming is like building a multilingual puzzle\n");
 	puts(text);
 	return (0);
 }
