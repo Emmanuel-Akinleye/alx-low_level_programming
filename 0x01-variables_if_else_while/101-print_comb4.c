278a4a26d210d9904ca6056578fd8e99a2116d30
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-17 18:33:54 +0100

    Print_Comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,28 @@
 
 int main(void)
 {
-int i,j,k;
-for (i = '0'; i <= '9'; i++)
+int i, j, k;
+
+for (i = 0; i < 8; i++)
 {
-for ( j = '0'; j < '9'; j++) 
+for (j = i + 1; j < 9; j++)
 {
-for ( k = '0'; k < '9'; k++)
+for (k = j + 1; k < 10; k++)
 {
-putchar(i);
-putchar(j);
-putchar(k);
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if ((i == 7) && (j == 8) && (k == 9))
+{
+break;
+}
+putchar('\0');
 putchar(',');
+putchar(' ');
 }
 }
 }
+
 return (0);
 }
 
