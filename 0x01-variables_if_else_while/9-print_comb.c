9d9bcd5d10c44e351a683f102a27c82fca3be6b3
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-17 15:24:52 +0100

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,16 @@
 int main(void)
 {
 int num;
-for (num = '0'; num <= '9'; num++)
+for (num = '0'; num <= '9'; num++) 
 {
 putchar(num);
+if ((num == '9'))
+{
+break;  
+}
 putchar(',');
 putchar(' ');
 }
+putchar('\n');
 return (0);
 }
