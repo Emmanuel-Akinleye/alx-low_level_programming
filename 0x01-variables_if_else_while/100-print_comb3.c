72f720ad7b3f897f9cc1bfbaba4c598520f23839
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-17 17:35:56 +0100

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,19 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 100; num++)
-{
-putchar((num / 10) + '0');
-putchar((num % 10) + '0');
-if ((num == 89))
-{
-break;
-}
-putchar(',');
-putchar(' ');
-}
-return (0);
+    int i;
+    int j; 
+    for(i = '0'; i <= '9'; ++i)
+    {
+        for(j = '0'; j <= '9'; ++j)
+        {
+                putchar(i);
+                putchar(j);
+                putchar(',');
+                putchar(' ');
+          
+
+        }
+    }
+  return (0);
 }
