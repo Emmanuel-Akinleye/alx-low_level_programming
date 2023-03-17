8a4ceb0a63c20fdc0ab9a87c9b0d351327c1a3c6
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-17 14:29:16 +0100

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success1)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'z'; alpha >= 'a'; alpha--) 
+{
+putchar(alpha);
+}
+return (0);
+}
