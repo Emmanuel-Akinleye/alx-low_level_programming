02071cd926425e1cc0d617b6338b991c96add148
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-21 09:47:13 +0100

    Prints the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,22 +5,28 @@
  *
  * Return: Always 0.
  */
+print_alphabet(void);
 int main(void)
 {
 print_alphabet();
 return (0);
 }
 /**
- * print_alphabet - Prints the alphabet
+ * print_alphabet - prints the alphabet
  *
- * Return: Void
+ * Return: Void.
 */
+
 int print_alphabet(void)
 {
 char alpha;
-for (alpha = 'a';  alpha <= 'z';  alpha++)
+for (int i = 0; i <= 10; i++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
 _putchar(alpha);
 _putchar('\n');
+
+}
 }
 }
