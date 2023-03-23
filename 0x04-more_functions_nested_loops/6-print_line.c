2edb59fb24fd1c5738c007fea81437e7a34af88a
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-23 18:40:08 +0100

    Shapes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Prints out '_'
+ * @n: This is the amount of times we're going to print out the line
+ * Return: We'll return n
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (n > 0)
+{
+_putchar('-');
+}
+else if (n <= 0)
+{
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
