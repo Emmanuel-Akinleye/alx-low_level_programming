583813895f28ea5b1d23c4c83161e95fe008fa22
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-27 21:54:17 +0100

    Errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_rev - prints a string in reverse order
+ * @s: this is thee char we'll print
+ *
+ * Return: nothing
+*/
 void print_rev(char *s)
 {
 int i, c = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-c++;    
+c++;
 }
 for (i = c - 1; i >= 0; i--)
 {
-  printf("%c", s[i]);
+printf("%c", s[i]);
 }
 printf("\n");
 }
