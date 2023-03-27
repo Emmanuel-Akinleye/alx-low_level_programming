117ce72f7e399b70d25518a125fa88db2d070cad
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-27 23:14:35 +0100

    Puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,19 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-
+/**
+ * puts2 - prints out everyother string to the stdout
+ * @str: this is pointer to a string;
+ *
+ * Return: nothing
+*/
 void puts2(char *str)
 {
 
 int i;
-for(i = 0; *str != '\0'; i++)
+for (i = 0; *str != '\0'; i++)
 {
-if(i % 2 == 0)
+if (i % 2 == 0)
 {
 printf("%c", *str);
 }
