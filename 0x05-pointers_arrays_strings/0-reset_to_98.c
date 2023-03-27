aae3ee8378e31e8360f72f8c63b63c2652e8d43d
Emmanuel-Akinleye <hazergames908@gmail.com>
2023-03-27 12:09:51 +0100

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Reset the integer
+ * @n:  a pointer the integer we want to set to 402
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
